2008e6106727a6babf3639c168bd242e801424f6
Denzel <jdenzelmab@gmail.com>
2023-10-07 19:34:55 -0400

    Added handleSubmit function LoginForm component

--- client/src/components/LogInForm.js
+++ client/src/components/LogInForm.js
@@ -6,5 +6,23 @@ function LoginForm({ onLogin }) {
     const [errors, setErrors] = useState([]);
     const [loading, setLoading] = useState(false);
 
-    
+    function handleSubmit(e) {
+        e.preventDefault();
+        setErrors([]);
+        setLoading(true);
+        fetch("/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, password }),
+        }).then((r) => {
+            setLoading(false);
+            if (r.ok) {
+                r.json().then((user) => onLogin(user));
+            } else {
+                r.json().then((err) => setErrors(err.errors));
+            }
+        });
+    }
 }

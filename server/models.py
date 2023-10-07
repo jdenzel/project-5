5463d76fcd0a19356facd874ce71a9313930cba7
Denzel <jdenzelmab@gmail.com>
2023-10-06 23:46:11 -0400

    Added password authenticator and finished User model

--- server/models.py
+++ server/models.py
@@ -1,3 +1,4 @@
+from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 
@@ -5,7 +6,23 @@ from config import db, bcrypt
 
 # Models go here!
 
+#User table
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(26))
+    name = db.Column(db.String())
+    _password_hash = db.Column(db.String(128))
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError
     
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))

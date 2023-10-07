afe9da42190a9da85c72f399d739f465835d9ea9
Denzel <jdenzelmab@gmail.com>
2023-10-07 19:30:56 -0400

    Added radio for player or staff input

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -6,6 +6,7 @@ function SignUpForm({ onLogin }) {
     const [passwordConfirmation, setPasswordConfirmation] = useState("");
     const [firstname, setFirstname] = useState("");
     const [lastname, setLastname] = useState("");
+    const [player_staff, setPlayer_staff] = useState("");
     const [errors, setErrors] = useState([]);
     const [loading, setLoading] = useState(false);
 
@@ -25,6 +26,7 @@ function SignUpForm({ onLogin }) {
                 password_confirmation: passwordConfirmation,
                 firstname,
                 lastname,
+                player_staff,
                 }),
             }).then((r) => {
                 setLoading(false);
@@ -87,6 +89,31 @@ function SignUpForm({ onLogin }) {
                     value={lastname}
                     onChange={(e) => setLastname(e.target.value)}
                 />
+            </div>
+            <div>
+                <label>Player or Staff</label>
+                <div>
+                    <label>
+                        <input
+                            type="radio"
+                            value="player"
+                            checked={player_staff === "player"}
+                            onChange={(e) => setPlayer_staff(e.target.value)}
+                        />
+                        Player
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <input
+                            type="radio"
+                            value="staff"
+                            checked={player_staff === "staff"}
+                            onChange={(e) => setPlayer_staff(e.target.value)}
+                        />
+                        Staff
+                    </label>
+                </div>
             </div>
             <div>
                 <button type="submit">{loading ? "Loading..." : "Sign Up"}</button>

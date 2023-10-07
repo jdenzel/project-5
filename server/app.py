6a8efd3ed63d10117caaf927d43f26a069256e1a
Denzel <jdenzelmab@gmail.com>
2023-10-07 18:55:19 -0400

    Added Logout route

--- server/app.py
+++ server/app.py
@@ -69,11 +69,19 @@ class Login(Resource):
             return user.to_dict(), 200
         else:
             return {'error': 'Unauthorized'}, 401
-        
+
+class Logout(Resource):
+    def delete(self):
+        if session.get('user_id'):
+            session['user_id'] = None
+            return {}, 204
+        else:
+            return {'error': 'Unauthorized'}, 401
         
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
 api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 
 if __name__ == '__main__':

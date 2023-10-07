3bfd45c4d7b1905af9fcec92aa9f66ceef4d0632
Denzel <jdenzelmab@gmail.com>
2023-10-06 23:43:37 -0400

    Created User class

--- server/models.py
+++ server/models.py
@@ -1,6 +1,11 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 
-from config import db
+from config import db, bcrypt
 
 # Models go here!
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    

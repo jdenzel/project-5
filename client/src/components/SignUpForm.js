69da0ca38c24caa2fb9da9bd153c782772f8c913
Denzel <jdenzelmab@gmail.com>
2023-10-07 19:01:50 -0400

    Added handleSubmit function to SignUpForm component

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -9,5 +9,32 @@ function SignUpForm({ onLogin }) {
     const [errors, setErrors] = useState([]);
     const [loading, setLoading] = useState(false);
 
+    function handleSubmit(e) {
+        e.preventDefault();
+        setErrors([]);
+        setLoading(true);
+        fetch("/signup", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                
+                username,
+                password,
+                password_confirmation: passwordConfirmation,
+                firstname,
+                lastname,
+                }),
+            }).then((r) => {
+                setLoading(false);
+                if (r.ok) {
+                    r.json().then((user) => onLogin(user));
+                } else {
+                    r.json().then((err) => setErrors(err.errors));
+                }
+            });
+    }
+
     
 }

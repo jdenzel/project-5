ec1d414580d85f41d8e826910afea49285864dcf
Denzel <jdenzelmab@gmail.com>
2023-10-12 13:53:36 -0400

    Added functions to handle patch and editing of UserProfile

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -2,12 +2,59 @@ import { useEffect, useState } from "react";
 
 function UserProfile() {
     const [user_profile, setUser_profile] = useState([]);
+    const [isEditing, setIsEditing] = useState(false);
+    const [form, setForm] = useState({
+        first_name: "",
+        last_name: "",
+        bio: "",
+        image_url: "",
+        jersey_number: "",
+    });
 
     useEffect(() => {
         fetch("/user_profile")
           .then((r) => r.json())
           .then(setUser_profile);
       }, []);
+
+    const handleInput = (e) => {
+        const key = e.target.name;
+        const value = e.target.value;
+        setForm({
+            ...form,
+            [key]: value,
+        });
+    };
+
+    const handleEdit = () => {
+        setForm({
+            first_name: user_profile.first_name,
+            last_name: user_profile.last_name,
+            bio: user_profile.bio,
+            image_url: user_profile.image_url,
+            jersey_number: user_profile.jersey_number,
+        });
+        setIsEditing(true);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetch("/user_profile", {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(form),
+        })
+        .then((r) => r.json())
+        .then((updatedUser) => {
+            setUser_profile(updatedUser);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    };
+    
 
     return (
         <div>
